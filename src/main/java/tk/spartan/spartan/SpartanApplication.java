327d942cec18d2be62dd1be35e257beacc02ad98
couler <couler@interia.pl>
2018-12-31 01:32:46 +0100

    first commit

---
+++ src/main/java/tk/spartan/spartan/SpartanApplication.java
@@ -0,0 +1,14 @@
+package tk.spartan.spartan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartanApplication.class, args);
+	}
+
+}
+
